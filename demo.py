d413e5ac85605e4becee8a1db7e09a226c72b7db
sumitlearnbay <140936314+sumitlearnbay@users.noreply.github.com>
2024-05-09 19:47:16 +0530

    demo

---
+++ demo.py
@@ -0,0 +1,8 @@
+# helloworld_project/urls.py
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('hello.urls')),
+]
